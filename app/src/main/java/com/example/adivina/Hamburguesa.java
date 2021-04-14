a4715d49895697da26ef0a310fc735f4339b13a2
alexlfz <alexlopezf365@gmail.com>
2021-04-14 17:19:24 +0200

    Cambio 6

---
+++ app/src/main/java/com/example/adivina/Hamburguesa.java
@@ -0,0 +1,17 @@
+package com.example.adivina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hamburguesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hamburguesa);
+
+
+
+    }
+}
