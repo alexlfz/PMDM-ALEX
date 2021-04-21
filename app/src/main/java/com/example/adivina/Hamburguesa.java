8a1967c21c4808864110829df4db3bd5f2f7c99b
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-04-21 16:41:20 +0200

    Mejoras en diseño y realizar pedido

--- app/src/main/java/com/example/adivina/Hamburguesa.java
+++ app/src/main/java/com/example/adivina/Hamburguesa.java
@@ -5,22 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class Hamburguesa extends AppCompatActivity {
 
-    CheckBox c1,c2,c3,c4,c5,c6;
+    CheckBox cPollo, cTofu, cTernera, cPatatasPeq, cPatatasMed, cPatatasGra;
     Button botonRealizar;
     float precioFinal = 0f;
-    ToggleButton toggleButton;
+    ToggleButton tEnsalada;
     String mensaje = "Hamburguesa: ";
-    Spinner spinner;
+    Spinner sQuesos;
+    SeekBar puntoCarne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,69 +48,89 @@ public class Hamburguesa extends AppCompatActivity {
         AlertDialog titulo = alerta.create();
         titulo.setTitle("Veganos NO");
 
-        c1=findViewById(R.id.checkBox1);
-        c2=findViewById(R.id.checkBox2);
-        c3=findViewById(R.id.checkBox3);
-        c4=findViewById(R.id.checkBox4);
-        c5=findViewById(R.id.checkBox5);
-        c6=findViewById(R.id.checkBox6);
-        toggleButton=findViewById(R.id.toggleButton);
-        spinner=findViewById(R.id.spinner2);
-        
+        cPollo =findViewById(R.id.checkBox1);
+        cTofu =findViewById(R.id.checkBox2);
+        cTernera =findViewById(R.id.checkBox3);
+        cPatatasPeq =findViewById(R.id.checkBox4);
+        cPatatasMed =findViewById(R.id.checkBox5);
+        cPatatasGra =findViewById(R.id.checkBox6);
+        tEnsalada =findViewById(R.id.toggleButton);
+        sQuesos =findViewById(R.id.spinner2);
+
+
+        puntoCarne = findViewById(R.id.seekBar3);
+
+        /*
+        sQuesos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mensaje += "Queso " + parent.getItemAtPosition(position).toString() + ", ";
+                precioFinal += 2f;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mensaje += "Sin queso, ";
+            }
+        });*/
         
         View.OnClickListener manejador = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(c3.isChecked()){
+                mensaje = "Hamburguesa: ";
+                precioFinal = 0;
+
+                if(cTernera.isChecked()){
                     precioFinal += 8.5f;
-                    mensaje += "Ternera, ";
+                    mensaje += "Ternera ";
                 }
-                if(c1.isChecked()){
+                if(cPollo.isChecked()){
                     precioFinal += 7.5f;
-                    mensaje += "Pollo, ";
+                    mensaje += "Pollo ";
+                }
+                if(cTofu.isChecked()){
+                    titulo.show();
+                    return;
                 }
 
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        mensaje += "Queso " + parent.getItemAtPosition(position).toString() + ", ";
-                        precioFinal += 2f;
-                    }
+                switch(puntoCarne.getProgress()) {
+                    case 0:
+                        mensaje += "poco hecha, ";
+                        break;
+                    case 1:
+                        mensaje += "medio ";
+                        break;
+                    case 2:
+                        mensaje += "quemada ";
+                        break;
+                }
 
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                        mensaje += "Sin queso, ";
-                    }
-                });
 
-                if(c4.isChecked()){
+                mensaje += sQuesos.getSelectedItem().toString();
+
+
+                if(cPatatasPeq.isChecked()){
                     precioFinal += 1;
                     mensaje += "patatas pequeñas, ";
                 }
-                if(c5.isChecked()){
+                if(cPatatasMed.isChecked()){
                     precioFinal += 1.5f;
                     mensaje += "patatas medianas, ";
                 }
-                if(c6.isChecked()){
+                if(cPatatasGra.isChecked()){
                     precioFinal += 2.25f;
                     mensaje += "patatas grandes, ";
                 }
 
-                if(toggleButton.isChecked()){
+                if(tEnsalada.isChecked()){
                     precioFinal += 1.50f;
                     mensaje += "y ensalada.";
                 }else{
                     mensaje += "sin ensalada.";
                 }
 
-                if(c2.isChecked()){
-                    titulo.show();
-                }else{
-                    Toast.makeText(getApplicationContext(),mensaje + "\nPrecio final: " + precioFinal + "€",Toast.LENGTH_LONG).show();
-                    precioFinal = 0f;
-                    mensaje = "";
-                }
+                Toast.makeText(getApplicationContext(),mensaje + "\nPrecio final: " + precioFinal + "€",Toast.LENGTH_LONG).show();
 
             }
         };
