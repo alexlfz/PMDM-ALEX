70b302fe852e8bb35d31a23d5a5a4185547c40b0
alexlfz <alexlopezf365@gmail.com>
2021-04-28 17:19:20 +0200

    Calculadora layaouts

--- app/src/main/java/com/example/adivina/Calculadora1.java
+++ app/src/main/java/com/example/adivina/Calculadora1.java
@@ -2,13 +2,41 @@ package com.example.adivina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Calculadora1 extends AppCompatActivity {
 
+    private Button bSeleccionar;
+    int LAUNCH_GETNUMBER_TOP = 1;
+    int LAUNCH_GETNUMBER_DOWN = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora1);
+
+        Intent i = new Intent(this, Calculadora2.class);
+
+        View.OnClickListener elQueManeja = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(i, LAUNCH_GETNUMBER_TOP);
+            }
+        };
+
+        View.OnClickListener elQueManeja2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(i, LAUNCH_GETNUMBER_DOWN);
+            }
+        };
+
+        bSeleccionar = findViewById(R.id.bSeleccionar1);
+        bSeleccionar.setOnClickListener(elQueManeja);
+
+        bSeleccionar = findViewById(R.id.bSeleccionar2);
+        bSeleccionar.setOnClickListener(elQueManeja2);
     }
 }
--- app/src/main/java/com/example/adivina/Calculadora2.java
+++ app/src/main/java/com/example/adivina/Calculadora2.java
@@ -2,13 +2,27 @@ package com.example.adivina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Calculadora2 extends AppCompatActivity {
+
+    private TextView tNumero;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora2);
+
+        tNumero = findViewById(R.id.tNumero);
+
+
+       /* Intent returnIntent = new Intent();
+        returnIntent.putExtra("result",result);
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
+        */
     }
 }
