ccb131fef91e63fdbdf1d0131f1330758d4f0b7a
alexlfz <alexlopezf365@gmail.com>
2021-05-04 17:10:46 +0200

    Commit 1

---
+++ app/src/main/java/com/example/adivina/Calculadora3.java
@@ -0,0 +1,14 @@
+package com.example.adivina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculadora3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora3);
+    }
+}
