c31c6c53a941dc66f29875c61d7af06277704094
alexlfz <alexlopezf365@gmail.com>
2021-05-25 16:33:21 +0200

    Realizar llamada

--- app/src/main/java/com/example/adivina/Permisos.java
+++ app/src/main/java/com/example/adivina/Permisos.java
@@ -38,25 +38,19 @@ public class Permisos extends AppCompatActivity {
             }
         };
 
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.CALL_PHONE)
-                    != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.CALL_PHONE)) {
-
-                } else {
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.CALL_PHONE},
-                            MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                }
-            }
-        }
 
         View.OnClickListener elQueManeja2 = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                callPhone();
+                int permiso = ContextCompat.checkSelfPermission(Permisos.this, Manifest.permission.CALL_PHONE);
+                if(permiso != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "No tiene permisos de llamada", Toast.LENGTH_LONG).show();
+                    ActivityCompat.requestPermissions(Permisos.this, new String[]{Manifest.permission.CALL_PHONE},255);
+                }else{
+                    Intent i2 = new Intent(Intent.ACTION_CALL);
+                    i2.setData(Uri.parse("tel:691641671"));
+                    startActivity(i2);
+                }
             }
         };
 
@@ -68,26 +62,5 @@ public class Permisos extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    callPhone();
-                } else {
-                    System.out.println("El usuario ha rechazado el permiso");
-                }
-                return;
-            }
-        }
-    }
 
-    public void callPhone() {
-        Intent intent = new Intent(Intent.ACTION_CALL);
-        intent.setPackage("com.android.phone");
-        intent.setData(Uri.parse("tel:" + "691641671"));
-        startActivity(intent);
-    }
 }
