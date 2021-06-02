9fe83e883bf46271cc7edbd080e9a3c660acde7d
alexlfz <alexlopezf365@gmail.com>
2021-06-02 17:18:58 +0200

    Primeros pasos adapter

---
+++ app/src/main/java/com/example/adivina/ListadoAdapter.java
@@ -0,0 +1,14 @@
+package com.example.adivina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_adapter);
+    }
+}
