1cd7eecdc45c19789fd6485a2e548982e1990d3c
alexlfz <alexlopezf365@gmail.com>
2021-06-08 21:07:58 +0200

    LISTADO EJERCICIO 1

--- app/src/main/java/com/example/adivina/ListadoActividad.java
+++ app/src/main/java/com/example/adivina/ListadoActividad.java
@@ -1,14 +1,55 @@
 package com.example.adivina;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ListadoActividad extends AppCompatActivity {
+
+    ListadoAdapter adapter;
+    ArrayList<ArrayList<String>> info = new ArrayList<>();
+    ArrayList<String> filas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_actividad);
+
+        RecyclerView recyclerView = findViewById(R.id.listView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ListadoAdapter(this);
+
+        /*adapter.setClickListener(new U12000_Adaptador.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //Toast.makeText(getApplicationContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        recyclerView.setAdapter(adapter);
+
+        cargarDatos(info, filas);
+
+        adapter.addData(info);
+    }
+
+    private void cargarDatos(ArrayList<ArrayList<String>> info, ArrayList<String> filas) {
+        filas.add("RMA");
+        filas.add("0");
+        filas.add("4");
+        filas.add("FCB");
+        info.add(filas);
+        filas.removeAll(filas);
+        filas.add("VAL");
+        filas.add("1");
+        filas.add("2");
+        filas.add("SEV");
+        info.add(filas);
     }
 }
--- app/src/main/java/com/example/adivina/ListadoAdapter.java
+++ app/src/main/java/com/example/adivina/ListadoAdapter.java
@@ -1,14 +1,83 @@
 package com.example.adivina;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class ListadoAdapter extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListadoAdapter extends RecyclerView.Adapter<ListadoAdapter.ViewHolder> {
+
+    private List<ArrayList<String>> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    ListadoAdapter(Context context) {
+        this.mInflater = LayoutInflater.from(context);
+        mData = new ArrayList<ArrayList<String>>();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listado_adapter);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.activity_listado_adapter, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ArrayList<String> linea = mData.get(position);
+        holder.tvInfoLocal.setText(linea.get(0));
+        holder.tvScoreLocal.setText(linea.get(1));
+        holder.tvScoreVisitante.setText(linea.get(2));
+        holder.tvInfoVisitante.setText(linea.get(3));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void addData(ArrayList<ArrayList<String>> info) {
+        mData.addAll(info);
+        notifyDataSetChanged();
+    }
+
+    ArrayList<String> getItem(int id) {
+        return mData.get(id);
+    }
+
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvInfoLocal;
+        TextView tvInfoVisitante;
+        TextView tvScoreLocal;
+        TextView tvScoreVisitante;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            tvInfoLocal = itemView.findViewById(R.id.idEquipoLocal);
+            tvInfoVisitante = itemView.findViewById(R.id.idEquipoVisitante);
+            tvScoreLocal = itemView.findViewById(R.id.idScoreLocal);
+            tvScoreVisitante = itemView.findViewById(R.id.idScoreVisitante);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
     }
 }
