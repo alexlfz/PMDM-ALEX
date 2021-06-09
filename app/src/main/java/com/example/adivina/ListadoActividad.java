c59859767fd11eed8e1dc1dd9a08c56ef1095c90
alexlfz <alexlopezf365@gmail.com>
2021-06-09 14:12:55 +0200

    LISTADO EJERCICIO 1.4

--- app/src/main/java/com/example/adivina/ListadoActividad.java
+++ app/src/main/java/com/example/adivina/ListadoActividad.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -14,7 +15,6 @@ public class ListadoActividad extends AppCompatActivity {
 
     ListadoAdapter adapter;
     ArrayList<ArrayList<String>> info = new ArrayList<>();
-    ArrayList<String> filas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +25,58 @@ public class ListadoActividad extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new ListadoAdapter(this);
 
-        /*adapter.setClickListener(new U12000_Adaptador.ItemClickListener() {
+        Intent i = new Intent(this, ListadoActividad2.class);
+        adapter.setClickListener(new ListadoAdapter.ItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                //Toast.makeText(getApplicationContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+                i.putExtra("Partido", adapter.getItem(position));
+                startActivity(i);
             }
-        });*/
+        });
 
         recyclerView.setAdapter(adapter);
 
-        cargarDatos(info, filas);
+        cargarDatos(info);
 
         adapter.addData(info);
     }
 
-    private void cargarDatos(ArrayList<ArrayList<String>> info, ArrayList<String> filas) {
-        filas.add("RMA");
-        filas.add("0");
-        filas.add("4");
-        filas.add("FCB");
-        info.add(filas);
-        filas.removeAll(filas);
-        filas.add("VAL");
-        filas.add("1");
-        filas.add("2");
-        filas.add("SEV");
-        info.add(filas);
+    private void cargarDatos(ArrayList<ArrayList<String>> info) {
+
+        ArrayList<String> fila1 = new ArrayList<>();
+        ArrayList<String> fila2 = new ArrayList<>();
+        ArrayList<String> fila3 = new ArrayList<>();
+        ArrayList<String> fila4 = new ArrayList<>();
+        ArrayList<String> fila5 = new ArrayList<>();
+
+        fila1.add("RMA");
+        fila1.add("0");
+        fila1.add("4");
+        fila1.add("FCB");
+        info.add(fila1);
+
+        fila2.add("VAL");
+        fila2.add("1");
+        fila2.add("2");
+        fila2.add("SEV");
+        info.add(fila2);
+
+        fila3.add("ATM");
+        fila3.add("3");
+        fila3.add("1");
+        fila3.add("MLG");
+        info.add(fila3);
+
+        fila4.add("VIL");
+        fila4.add("0");
+        fila4.add("3");
+        fila4.add("ZGZ");
+        info.add(fila4);
+
+        fila5.add("CEL");
+        fila5.add("5");
+        fila5.add("1");
+        fila5.add("DEP");
+        info.add(fila5);
     }
 }
